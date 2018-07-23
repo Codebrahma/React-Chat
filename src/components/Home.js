ea772903df75085005c4fcad9a7a3040f0eeeca8
sai <saikrishna@codebrahma.com>
2018-07-23 13:39:29 +0530

    test(test): Added enzyme,mocha,chai testing frameworks with one sample test case

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => (
+  <div className="home">
+    <div className="home">I am at Home 1</div>
+    <div>I am at Home 2</div>
+  </div>
+)
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './index.scss';
+import Home from './components/Home'
 
 const Index = () => (
   <div className={styles.cb}>
+  <Home />
     Hello React Chat!
   </div>
 );
