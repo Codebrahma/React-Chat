4ea8f79f4fbb3de48cdc94b94c2efad012ae2efa
rishichawda <chawda.rishii@gmail.com>
2018-07-26 20:24:18 +0530

    resolve merge conflicts

--- src/examples/Home.js
+++ src/examples/Home.js
@@ -12,18 +12,6 @@ import ChatListItem1 from './exampleChat1/ChatListItem1'
 import ChatListSearch1 from './exampleChat1/ChatListSearch1'
 import ChatWindowHeader from './chat-window/ChatWindowHeader'
 import WindowIndex from './chat-window/index'
-  ChatListProvider,
-  // ChatListHeader,
-  ChatList,
-  ChatListSearch,
-} from '../components'
-// import ChatListHeader1 from './exampleChat1/ChatListHeader1'
-// import ChatList1 from './exampleChat1/ChatList1'
-// import ChatListItem1 from './exampleChat1/ChatListItem1'
-// import ChatListSearch1 from './exampleChat1/ChatListSearch1'
-
-import ExampleChat2 from './exampleChat2';
-// Import demo page for chat themes
 import DemoPage from './demoPage';
 
 class Home extends Component {
@@ -42,29 +30,7 @@ class Home extends Component {
   }
   render() {
     return (
-      <div className="home">
-        <WindowIndex userData={userData}/>
-      </div>
-      <DemoPage />
-
-      // <ExampleChat2 />
-
-      // <ChatListProvider
-      //   userData={userData}
-      // />
-      // <div className="home">
-      //   {/* <ChatListProvider
-      //     userData={userData}
-      //     chatProviderClass="example-provider1"
-      //     handleSearchChange={this.handleSearchChange}
-      //   >
-      //   <ChatListHeader1 />
-      //   <ChatList
-      //     customChatListItem={ChatListItem1}
-      //     chatListClass="example-list1"
-      //   />
-      //   </ChatListProvider> */}
-      // </div>
+      <DemoPage/>
     )
   }
 }
