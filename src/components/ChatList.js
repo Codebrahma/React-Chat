c2247ffbe7631569b975156f69f42ed9c815aae3
sai <saikrishna@codebrahma.com>
2018-07-29 12:13:23 +0530

    feat(chatList): Added filter functionality on search

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -5,18 +5,20 @@ import ChatListItem from './ChatListItem'
 const ChatList = (props) => (
     <div className={props.chatListClass}>
       {
-        props.userData.map(item => (
-          <props.customChatListItem
-            key={item.id}
-            id={item.id}
-            avatar={item.avatar}
-            name={item.name}
-            onlineStatus={item.onlineStatus}
-            lastSeen={item.lastSeen}
-            {...props}
-            {...item}
-          />
-        ))
+        props.userData.length === 0
+          ? <span className='no-user-found'>No User Found</span>
+          : props.userData.map(item => (
+              <props.customChatListItem
+                key={item.id}
+                id={item.id}
+                avatar={item.avatar}
+                name={item.name}
+                onlineStatus={item.onlineStatus}
+                lastSeen={item.lastSeen}
+                {...props}
+                {...item}
+              />
+            ))
       }
     </div>
   )
