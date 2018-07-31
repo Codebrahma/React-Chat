639cdac42d3ad08b6bcab28e38feba78f3f15769
rishi chawda <chawda.rishii@gmail.com>
2018-08-01 01:49:18 +0530

    add storybook navigation to demopage

--- src/examples/demoPage/DemoPageNavbar.js
+++ src/examples/demoPage/DemoPageNavbar.js
@@ -7,6 +7,9 @@ const DemoPageNavbar = () => {
             <div className="demo-page-navbar-brand">
                 <strong>React Chat Components</strong>
             </div>
+            <div className="navbar-links">
+                <a href="http://localhost:9001/">Storybook</a>
+            </div>
         </div>
     )
 }
